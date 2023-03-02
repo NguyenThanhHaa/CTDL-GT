f56c782617dd82b669bea01e1ad783e524ce9d74
ThanhHa <119502362+NguyenThanhHaa@users.noreply.github.com>
2023-03-02 20:34:44 +0700

    Add files via upload

---
+++ Chuong 2/Ly thuyet/Quaylui.cpp
@@ -0,0 +1,13 @@
+//Cai dat thuat toan quay lui
+
+Ham quay lui(x)
+{
+	if (x không phai la giai phap)
+        Tra ve false;
+        
+    if (x la mot giai phap moi)
+        Them vào danh sách các giai phap; 
+        
+    Ham quay lui(mo rong/ trien khai them cho x) 
+}
+    
