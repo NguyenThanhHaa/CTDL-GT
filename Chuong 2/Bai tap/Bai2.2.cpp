e0c0c54930d754b7f3570f0d871ef2c4abe07327
ThanhHa <119502362+NguyenThanhHaa@users.noreply.github.com>
2023-03-03 22:42:55 +0700

    Add files via upload

---
+++ Chuong 2/Bai tap/Bai2.2.cpp
@@ -0,0 +1,21 @@
+// Bai 2.2
+// Cai dat ham de quy tinh n!
+
+#include <iostream>
+using namespace std;
+
+int Giaithua(int n)
+{	
+	if (n==0|| n==1)
+		return 1;
+	
+	else
+		return n*(Giaithua(n-1)); 
+ } 
+
+int main()
+{	int n; 
+	cout << "Nhap n: ";
+	cin >> n;
+	cout <<"Giai thua cua n: " << Giaithua(n); 
+ } 
