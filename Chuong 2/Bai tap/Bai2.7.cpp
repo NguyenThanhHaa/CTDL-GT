7e6d0193d403d1f4770b936bfba9baa9fb459b71
ThanhHa <119502362+NguyenThanhHaa@users.noreply.github.com>
2023-03-05 17:07:27 +0700

    Update Bai2.7.cpp

--- Chuong 2/Bai tap/Bai2.7.cpp
+++ Chuong 2/Bai tap/Bai2.7.cpp
@@ -1,9 +1,14 @@
 //Bai 2.7
 //Khai bao cau truc du lieu cho ds lien ket kep cac so nguyen
 
+struct Node
+{
+	int n;
+	Node *truoc, *sau;  
+ };
+ 
 struct Songuyen
 {
-	int x;
 	Node *dau, *cuoi; 
 	 
 }; 
